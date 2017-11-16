f0bd21f46c5483da805a4b48316f3b1054e71bc7
jordan-cbw <jordan@coolblueweb.com>
2017-11-16 12:17:37 -0800

    Mostly finished the install script

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # 1pass-cbw
 =====
 
-### Installation
+## Installation
 
+#### Mac OSX x64 install
 ```shell
 mkdir ~/Documents/1pass-cbw
 git clone https://github.com/jordan-cbw/1pass-cbw.git ~/Documents/1pass-cbw
 cd ~/Documents/1pass-cbw
-#./install.sh
+./install.sh mac
+```
+
+
+#### linux x32 install (Digital Ocean droplets)
+```shell
+mkdir ~/Documents/1pass-cbw
+git clone https://github.com/jordan-cbw/1pass-cbw.git ~/Documents/1pass-cbw
+cd ~/Documents/1pass-cbw
+./install.sh linux
 ```
--- install.sh
+++ install.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-if [ -f /usr/local/bin/jq ]
-then
+if [ "$#" -eq 1 ]
+ then
+  if [ -f /usr/local/bin/jq ]
+   then
     echo "jq found. Not installing."
-else
+   else
     echo "Copying jq to /usr/local/bin/"
-    cp ~/Documents/1pass-cbw/jq /usr/local/bin/
-fi
-
-if [ -f /usr/local/bin/op ]
-then
+    if [ "$1" = "mac" ]
+     then
+      cp ~/Documents/1pass-cbw/osx/jq /usr/local/bin/
+    fi
+    if [ "$1" = "linux" ]
+     then
+      cp ~/Documents/1pass-cbw/linux/jq /usr/local/bin/
+    fi
+  fi
+  if [ -f /usr/local/bin/op ]
+   then
     echo "op found. Not installing."
-else
+   else
     echo "Copying op to /usr/local/bin/"
-    cp ~/Documents/1pass-cbw/op /usr/local/bin/
+    if [ "$1" = "mac" ]
+     then
+      cp ~/Documents/1pass-cbw/osx/op /usr/local/bin/
+    fi
+    if [ "$1" = "linux" ]
+     then
+      cp ~/Documents/1pass-cbw/linux/op /usr/local/bin/
+    fi
+  fi
+ else
+  echo 'Error: "mac" or "linux"'
 fi
